28da2d29eaf265dd96591bb35617fc77178842f8
aortu22 <aortu@itba.edu.ar>
2023-04-14 16:45:02 -0300

    Implementation of bpm functions

--- src/bmp.c
+++ src/bmp.c
@@ -16,6 +16,32 @@ BMPHeader* cloneBmpHeader(BMPHeader* src) {
     return dest;
 }
 
+/* Sets the reserved1 byte of the header to the shadow label value */
+void labelBmpImage(BMPImage * bmp, uint16_t label){
+    header->reserved1 = label
+}
+
+/* Generates a BMP image from a deep copy of the header and a shadow copy of the data */
+BMPImage * generateImage(BMPHeader * header, uint8_t * data){
+    // Allocate memory for new BMPImage
+    BMPImage* bmp = malloc(sizeof(BMPImage));
+    if (!bmp) {
+        return NULL; // allocation failed
+    }
+
+    // Deep copy header
+    bmp->header = malloc(sizeof(BMPHeader));
+    if (!bmp->header) {
+        free(bmp); // free previously allocated memory
+        return NULL; // allocation failed
+    }
+    memcpy(bmp->header, header, sizeof(BMPHeader));
+
+    // Shadow copy data
+    bmp->data = data;
+
+    return bmp;
+}
 
 BMPImage* loadBmp(const char* path) {
     int fd = open(path, O_RDONLY);
@@ -87,6 +113,64 @@ void dumpBmpInverted(BMPImage* bmp) {
             fwrite(&padding_byte, sizeof(uint8_t), 1, stdout);
         }
     }
+}
+
+/* Dumps all bytes of a BMPImage into stdout. It can be piped into a file. */
+void dumpBmp(BMPImage* bmp){
+    // Dump header as-is
+    fwrite(bmp->header, bmp->header->data_offset, 1, stdout);
+
+    // Dump inverted image data
+    int width = bmp->header->width;
+    int height = bmp->header->height;
+    int padding = (4 - (width % 4)) % 4;  // BMP row padding is rounded up to a multiple of 4 bytes
+    uint8_t* data = bmp->data;
+
+    for (int y = height - 1; y >= 0; y--) {  // iterate over rows in reverse order
+        for (int x = 0; x < width; x++) {   // iterate over pixels in row
+            uint8_t pixel = *(data + y*(width + padding) + x);
+            fwrite(&pixel, sizeof(uint8_t), 1, stdout);
+        }
+        // write row padding
+        uint8_t padding_byte = 0x00;
+        for (int i = 0; i < padding; i++) {
+            fwrite(&padding_byte, sizeof(uint8_t), 1, stdout);
+        }
+    }
+}
+
+/* Dumps all bytes of a BMPImage into a file in specified path. If it doesn't exist, the file is created. */
+void dumpBmpToFile(BMPImage * bmp, char * path){
+    // Open file for writing
+    FILE* file = fopen(path, "wb");
+    if (file == NULL) {
+        fprintf(stderr, "Error: could not open file for writing\n");
+        return;
+    }
+
+    // Dump header as-is
+    fwrite(bmp->header, bmp->header->data_offset, 1, file);
+
+    // Dump image data
+    int width = bmp->header->width;
+    int height = bmp->header->height;
+    int padding = (4 - (width % 4)) % 4;  // BMP row padding is rounded up to a multiple of 4 bytes
+    uint8_t* data = bmp->data;
+
+    for (int y = height - 1; y >= 0; y--) {  // iterate over rows in reverse order
+        for (int x = 0; x < width; x++) {   // iterate over pixels in row
+            uint8_t pixel = *(data + y*(width + padding) + x);
+            fwrite(&pixel, sizeof(uint8_t), 1, file);
+        }
+        // write row padding
+        uint8_t padding_byte = 0x00;
+        for (int i = 0; i < padding; i++) {
+            fwrite(&padding_byte, sizeof(uint8_t), 1, file);
+        }
+    }
+
+    // Close file
+    fclose(file);
 }
 
 void freeBmp(BMPImage * bmp) {
